506be795b08131eef84c372629b6bf953d05a4e8
shakhoond <shayan.akhoond@gmail.com>
2016-04-14 11:02:52 +1200

    index.html

--- javascript/caculator.js
+++ javascript/caculator.js
@@ -1,8 +1,8 @@
-var key = document.querySelectorAll(".wrapper td");
-var result;
-var findResult;
+var key = document.querySelectorAll(".wrapper td"); //Select all table cells for calculator buttons
+var result;//dispaly the result
+
 	for(var i = 0; i < key.length; i++) {
-		key[i].onclick = (function(e) {
+		key[i].onclick = function(e) {
 			console.log(e);
 			var input = document.querySelector(".display td");
 			var inputVal = input.innerHTML;
@@ -10,19 +10,40 @@ var findResult;
 			console.log(printInput);
 
 
-
 			
-				if(printInput === "CE"){
-            input.innerHTML = printInput[printInput.length-1];
-				}else if (printInput === "AC") {
-				input.innerHTML = "";
-			}else if (printInput === "="){
-				result = input.innerHTML;
-				input.innerHTML = eval(result);
-			}else{
-			input.innerHTML += printInput; }
-			console.log(input)
-		})
-		
+			if(printInput === "CE"){
+				input.innerHTML = input.innerHTML.slice(0, -1);
+				
+			}else if (printInput === "AC") {
+				inputVal = "";
+			}
 
+
+			else if (printInput === "="){
+				result = inputVal;
+				input.innerHTML = myEval(result);
+			
+       			;
+        		}else{
+				input.innerHTML += printInput;
+      		}
+function myEval(inputValue)
+{//
+	var n = inputValue.indexOf("gt")//Find index of power
+	//If n == -1 it means the opoerator is something else so the Eval function will take care of it in Else block.
+	if (n > -1)
+	{
+		var number1 = inputValue.substring(0,n);
+		var number2 =  inputValue.substring(n+2, inputValue.length);
+		return Math.pow(number1, number2);
 	}
+else
+{
+return eval(inputValue);
+}
+}
+
+
+		}
+	}
+
